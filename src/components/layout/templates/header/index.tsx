2a1ef3ec95ec1188369785aca55c53eea91e77e7
Arturs <arturs.vanags320@gmail.com>
2023-08-31 15:54:55 +0300

    Added button, changed few properties.

--- src/components/layout/templates/header/index.tsx
+++ src/components/layout/templates/header/index.tsx
@@ -9,7 +9,8 @@ export default function SiteHeader() {
       <div className="container flex items-center h-16">
         <NavMenu />
         <div className="flex flex-1 items-center space-x-2 justify-end">
-          <Button variant="default"><Icons.Access className="h-4 w-4 mr-2" /> Access Images</Button>
+          <Button variant="default">Store</Button>
+          <Button variant="default"><Icons.Access className="h-4 w-4 mr-2" />Access Photos</Button>
           <Button asChild variant="ghost" size="icon"><Link href="/account"><Icons.User className="h-4 w-4" /></Link></Button>
           <Button asChild variant="ghost" size="icon"><Link href="/cart"><Icons.MyCart className="h-4 w-4" /></Link></Button>
         </div>
