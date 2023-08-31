87c7d35babc543531cde115ecc24c13fbf1c50c3
Arturs <arturs.vanags320@gmail.com>
2023-08-31 22:00:56 +0300

    modified:   src/components/layout/templates/index.tsx

--- src/components/layout/templates/index.tsx
+++ src/components/layout/templates/index.tsx
@@ -1,11 +1,11 @@
 import Footer from "@/components/layout/templates/footer"
-import Nav from "@/components/layout/templates/nav"
+import NavMenu from "@/components/layout/templates/header/navigation-menu"
 import React from "react"
 
 const Layout: React.FC = ({ children }) => {
   return (
     <div>
-      <Nav />
+      <NavMenu />
       <main className="relative">{children}</main>
       <Footer />
     </div>
