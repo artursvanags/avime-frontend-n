d47eabaa28059a46769138f4089ce1d218fd7cf6
Arturs <arturs.vanags320@gmail.com>
2023-09-13 16:40:23 +0300

    modified:   src/components/store/templates/ListProducts.tsx

--- src/components/store/templates/ListProducts.tsx
+++ src/components/store/templates/ListProducts.tsx
@@ -5,17 +5,7 @@ import { ProductCard } from "../components/ProductCard";
 export default function ListProducts() {
     const { products, isLoading } = useProducts()
   return (
-    <div>
-        
-      {isLoading && <span>Loading...</span>}
-      {products && !products.length && <span>No Products</span>}
-      {products && products.length > 0 && (
-        <div>
-          {products.map((product) => (
-        
-          ))}
-        </div>
-      )}
+    <div>test
     </div>
   );
 }
