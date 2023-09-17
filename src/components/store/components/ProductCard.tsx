608373a3f6b70ed9a8daad8e873a813399fa0a2f
Arturs <arturs.vanags320@gmail.com>
2023-09-17 13:22:21 +0300

    Updated components and installed badge.

--- src/components/store/components/ProductCard.tsx
+++ src/components/store/components/ProductCard.tsx
@@ -1,26 +1,48 @@
 import Image from "next/image";
+import { Product as MedusaProduct } from "@/types/medusa";
+import { PricedProduct } from "@medusajs/medusa/dist/types/pricing";
+import Link from "next/link";
+import Grid from "@/components/global/grid";
+import { Badge } from "@/components/ui/badge";
 
 type CardProp = {
-    img: string;
-    t: string;
-    price?: string;
-  };
-  
-export function ProductCard({img, t, price }: CardProp) {
-    return (
-      <div className="rounded border bg-stone-50 p-4 dark:bg-transparent">
-        <div className="space-y-2">
-            <Image
-              src={img}
-              loading="lazy"
-              fill
-              className="rounded-md object-cover"
-              alt="Image"
-            />
-          {t}
-          {price}
-        </div>
-      </div>
-    );
-  }
-  
+  products: (MedusaProduct | PricedProduct)[];
+  loading?: boolean;
+};
+
+export function ProductCard({ products, loading }: CardProp) {
+  return (
+    <>
+      {products?.map((p) => (
+        <Grid.Item key={p.id}>
+          <Link
+            href={`/products/${p.handle}`}
+            className=" relative inline-block h-full w-full rounded border bg-stone-50 p-6 hover:bg-stone-100 dark:bg-transparent dark:hover:bg-stone-900"
+          >
+            <div className="relative aspect-square">
+              <Image
+                src={p?.thumbnail || "/default-image.jpg"}
+                fill
+                className="rounded object-cover"
+                alt={p.title ?? "Product Title"}
+              />
+            </div>
+            <h3 className="pt-2 font-medium">{p.title}</h3>
+            {p.tags && p.tags.length > 0 && (
+              <div className="flex flex-row space-x-1 pt-1">
+                {p.tags?.map((tag) => (
+                  <Badge key={tag.id} variant={"secondary"}>
+                    {tag.value}
+                  </Badge>
+                ))}
+              </div>
+            )}
+            <div className="pt-2 text-justify text-sm text-muted-foreground">
+              {p.description}
+            </div>
+          </Link>
+        </Grid.Item>
+      ))}
+    </>
+  );
+}
--- src/components/store/templates/ListProducts.tsx
+++ src/components/store/templates/ListProducts.tsx
@@ -1,11 +1,21 @@
 "use client";
-import { useProducts, formatVariantPrice } from "medusa-react";
+
+import { useProducts } from "medusa-react";
 import { ProductCard } from "../components/ProductCard";
+import Grid from "@/components/global/grid";
 
 export default function ListProducts() {
-    const { products, isLoading } = useProducts()
+  const { products, isLoading } = useProducts();
+
   return (
-    <div>test
+    <div className="py-6">
+      {isLoading && <span>Loading...</span>}
+      {products && !products.length && <span>No Products</span>}
+      <Grid className="container grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+        {products && products.length > 0 ? (
+          <ProductCard products={products} loading={isLoading}/>
+        ) : null}
+      </Grid>
     </div>
   );
 }
