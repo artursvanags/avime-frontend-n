fceb237de1b0758709cda088713c4ac818fd2b7d
Arturs <arturs.vanags320@gmail.com>
2023-09-29 16:00:32 +0300

    Updated product page.

--- src/components/products/components/product-actions/index.tsx
+++ src/components/products/components/product-actions/index.tsx
@@ -1,31 +1,65 @@
-import { useProductActions } from "@/lib/context/product-context"
-import useProductPrice from "@/lib/hooks/use-product-price"
-import { PricedProduct } from "@medusajs/medusa/dist/types/pricing"
-import Button from "@/components/common/components/button"
-import OptionSelect from "@/components/products/components/option-select"
-import clsx from "clsx"
-import Link from "next/link"
-import React, { useMemo } from "react"
-import { Product } from "@/types/medusa"
+import { useProductActions } from "@/lib/context/product-context";
+import useProductPrice from "@/lib/hooks/use-product-price";
+import { PricedProduct } from "@medusajs/medusa/dist/types/pricing";
+import { Button } from "@/components/ui/button";
+import OptionSelect from "@/components/products/components/option-select";
+import clsx from "clsx";
+import Link from "next/link";
+import React, { useMemo, useState } from "react";
+import { useAccount } from "@/lib/context/account-context";
+import { useToast } from "@/components/ui/use-toast";
+import { ToastAction } from "@/components/ui/toast";
+import { cn } from "@/lib/utils";
+import { Spinner } from "@/config/icons";
 
 type ProductActionsProps = {
-  product: PricedProduct
-}
+  product: PricedProduct;
+};
 
 const ProductActions: React.FC<ProductActionsProps> = ({ product }) => {
   const { updateOptions, addToCart, options, inStock, variant } =
-    useProductActions()
-
-  const price = useProductPrice({ id: product.id!, variantId: variant?.id })
+    useProductActions();
+  const { customer, retrievingCustomer } = useAccount();
+  const price = useProductPrice({ id: product.id!, variantId: variant?.id });
+  const { toast } = useToast();
+  const [adding, setAdding] = useState(false);
+  const [added, setAdded] = useState(false);
+  const [buttonText, setButtonText] = useState("Add to cart");
 
   const selectedPrice = useMemo(() => {
-    const { variantPrice, cheapestPrice } = price
+    const { variantPrice, cheapestPrice } = price;
 
-    return variantPrice || cheapestPrice || null
-  }, [price])
+    return variantPrice || cheapestPrice || null;
+  }, [price]);
+
+  const handleAddToCart = () => {
+    setAdding(true);
+
+    // Call the addToCart function
+    addToCart();
+
+    // Simulate a delay of 2-3 seconds before marking as added
+    setTimeout(() => {
+      setAdding(false);
+      setAdded(true);
+      setButtonText("Added!");
+
+      toast({
+        variant: "success",
+        title: "Added to Cart!",
+        description: `You have successfully added ${product?.title}`,
+        action: (
+          <ToastAction asChild altText="View Cart">
+            <Link href="/cart">View Cart</Link>
+          </ToastAction>
+        ),
+      });
+    }, 2000); // Delay for 2 seconds (2000 milliseconds)
+  };
 
   return (
     <div className="flex flex-col gap-y-2">
+      {/* Product collection info*/}
       {product.collection && (
         <Link
           href={`/collections/${product.collection.handle}`}
@@ -34,32 +68,15 @@ const ProductActions: React.FC<ProductActionsProps> = ({ product }) => {
           {product.collection.title}
         </Link>
       )}
-      <h3 className="text-xl-regular">{product.title}</h3>
+      {/* Product heading */}
+      <h1 className="pt-1 font-heading text-5xl">{product.title}</h1>
 
-      <p className="text-base-regular">{product.description}</p>
-
-      {product.variants.length > 1 && (
-        <div className="my-8 flex flex-col gap-y-6">
-          {(product.options || []).map((option) => {
-            return (
-              <div key={option.id}>
-                <OptionSelect
-                  option={option}
-                  current={options[option.id]}
-                  updateOption={updateOptions}
-                  title={option.title}
-                />
-              </div>
-            )
-          })}
-        </div>
-      )}
-
-      <div className="mb-4">
-        {selectedPrice ? (
-          <div className="flex flex-col text-gray-700">
+      {/* Product pricing */}
+      {!retrievingCustomer && customer ? (
+        selectedPrice ? (
+          <div className="flex flex-col text-3xl font-bold">
             <span
-              className={clsx("text-xl-semi", {
+              className={clsx("", {
                 "text-rose-600": selectedPrice.price_type === "sale",
               })}
             >
@@ -81,14 +98,53 @@ const ProductActions: React.FC<ProductActionsProps> = ({ product }) => {
           </div>
         ) : (
           <div></div>
-        )}
+        )
+      ) : (
+        <>A$ Please Login to view Pricing</>
+      )}
+      {}
+      <div className="whitespace-pre-line pt-3 text-justify text-sm">
+        {product.description}
       </div>
 
-      <Button onClick={addToCart}>
-        {!inStock ? "Out of stock" : "Add to cart"}
-      </Button>
-    </div>
-  )
-}
+      {/* Product options, if multiple */}
+      {product.variants.length > 1 && (
+        <div className="my-8 flex flex-col gap-y-6">
+          {(product.options || []).map((option) => {
+            return (
+              <div key={option.id}>
+                <OptionSelect
+                  option={option}
+                  current={options[option.id]}
+                  updateOption={updateOptions}
+                  title={option.title}
+                />
+              </div>
+            );
+          })}
+        </div>
+      )}
 
-export default ProductActions
+      {/* Product button*/}
+      <div className="flex flex-col pt-3">
+        {!retrievingCustomer && customer ? (
+          <Button
+            size={"xl"}
+            onClick={handleAddToCart}
+            disabled={adding}
+          >
+            {adding
+              ? <><Spinner className="animate-spin w-5 h-5 mr-2"/>Adding...</>
+              : !inStock
+              ? "Out of stock"
+              : buttonText}
+          </Button>
+        ) : (
+          <>A$ Please Login to view Pricing</>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProductActions;
--- src/components/products/templates/index.tsx
+++ src/components/products/templates/index.tsx
@@ -1,17 +1,10 @@
 "use client";
 
 import { ProductProvider } from "@/lib/context/product-context";
-import { useIntersection } from "@/lib/hooks/use-in-view";
-import ProductTabs from "@/components/products/components/product-tabs";
-import RelatedProducts from "@/components/products/components/related-products";
-import ProductInfo from "@/components/products/templates/product-info";
 import React, { useRef } from "react";
 import ImageGallery from "../components/image-gallary";
-import MobileActions from "../components/mobile-actions";
 import { PricedProduct } from "@medusajs/medusa/dist/types/pricing";
-import { findCheapestVariantPrice as price } from "@/lib/util/get-price";
-import { useAccount } from "@/lib/context/account-context";
-import AddToCart from "../components/AddToCart";
+import ProductActions from "../components/product-actions";
 
 type ProductTemplateProps = {
   product: PricedProduct;
@@ -19,7 +12,6 @@ type ProductTemplateProps = {
 
 const ProductTemplate: React.FC<ProductTemplateProps> = ({ product }) => {
   const info = useRef<HTMLDivElement>(null);
-  const { customer, retrievingCustomer } = useAccount();
 
   return (
     <ProductProvider product={product}>
@@ -31,27 +23,7 @@ const ProductTemplate: React.FC<ProductTemplateProps> = ({ product }) => {
           className="flex w-full flex-col py-8 small:sticky small:top-20 small:max-w-[344px] small:py-0 medium:max-w-[400px]"
           ref={info}
         >
-          <h1 className="pt-1 font-heading text-5xl">{product.title}</h1>
-          <div className=" text-4xl font-medium text-muted-foreground">
-            {!retrievingCustomer && customer ? (
-              <>A$ {price(product)}</>
-            ) : (
-              <>A$ Please Login to view Pricing</>
-            )}
-          </div>
-          <div className="whitespace-pre-line pt-3 text-justify text-sm">
-            {product.description}
-          </div>
-
-          <div className="pt-1 text-sm text-muted-foreground">
-            {!retrievingCustomer && customer ? (
-              <>
-                <AddToCart product={product}/>
-              </>
-            ) : (
-              <AddToCart disabled />
-            )}
-          </div>
+          <ProductActions product={product} />
         </div>
       </div>
     </ProductProvider>
