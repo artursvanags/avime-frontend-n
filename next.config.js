44f89b35f134819632e5bb332047cf1e1ee8eb57
Arturs <arturs.vanags320@gmail.com>
2023-10-11 12:44:05 +0300

    Updated packages and settings.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@ const { withStoreConfig } = require("./store-config");
 const store = require("./store.config.json");
 
 module.exports = withStoreConfig({
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.externals.push({
+      "@aws-sdk/signature-v4-multi-region":
+        "commonjs @aws-sdk/signature-v4-multi-region",
+    });
+
+    return config;
+  },
   experimental: {
     serverActions: true,
   },
@@ -13,7 +21,8 @@ module.exports = withStoreConfig({
       "localhost",
       "medusa-server-testing.s3.amazonaws.com",
       "images.unsplash.com",
-      "cdn.avime.au"
+      "cdn.avime.au",
+      "avime-cdn.syd1.digitaloceanspaces.com",
     ],
   },
 });
